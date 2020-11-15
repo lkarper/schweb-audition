7f6bf1c9824616e5b19a12cc8854ddb1ee3f7903
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-14 20:13:05 -0500

    Initial commit

---
+++ coded-message.js
@@ -0,0 +1,51 @@
+/* 
+    This function takes a coded message argument (codedMessage) and returns a decoded message using these steps:
+    1. It takes the string 'abcdefghijklmnopqrstuvwxyz_' (baseKeyString)
+    2. It sorts the characters in the string by the number of times each character appears in the codedMessage argument (in descending order)
+    3. It removes all the characters from this sorted string after (and including) the _
+    4. It then returns the resulting word (decodedMessage)
+*/
+
+const decodeMessage = (codedMessage) => {
+    const baseKeyString = 'abcdefghijklmnopqrstuvwxyz_';
+    
+    const numCharOccurances = {};
+
+    // Initialize the count for each character at 0
+    [...baseKeyString].forEach(char => numCharOccurances[char] = 0);
+    
+    // Iterate through the coded message and count the number of occurances of each character
+    [...codedMessage].forEach(char => numCharOccurances[char] = numCharOccurances[char] + 1);
+    
+    /* 
+        Create an array that stores the number of times each character occurs and 
+        sort the array by occurances in descending order 
+    */
+    const charOccurancesArray = Object.keys(numCharOccurances)
+        .map(key => {
+            return {
+                key,
+                occurances: numCharOccurances[key],
+            };
+        })
+        .sort((a, b) => b.occurances - a.occurances);
+    
+    // Find the index of the object with the key equal to '_'
+    const endIndex = charOccurancesArray.findIndex(obj => obj.key === '_');
+
+    let decodedMessage = ''; 
+
+    /*
+        Slice the sorted array of stored character occurances to create an array with only those characters that
+        occur more frequently than '_' and concatinate each of those characters, 
+        in descending order of occurances, to reveal the decoded message 
+    */
+    charOccurancesArray.slice(0, endIndex).forEach(obj => decodedMessage += obj.key);
+    
+    return decodedMessage;
+}
+
+const codedMessage = 'cwrxwzbgickpjbp_svnudntddwdqbfgzyiqpuxddmpvyfquosmicfzkjekxzchngpqaksafulateukuwomdrwza_n_ptzktjzcuibnebe_tqessrzqewgkadrkvtyznaupodanwazopg_fijcoojojbsolr_ejesukzc_quochdnmti_lkvrsegyieqlqysuxdvetkqtkhxaiypfdiddztlicjurnllriopdtuuzpryrsepfydyeg_xkr_ruxp_lgqesysidfsygztwrba_ay_gaqqklbrvr_lbhawjraqujfxptmuvqfzklfodgaqrnhjravksjwemoosdlxtvw_qspxmlvqryusfixzlkb_p_c_tepzozzwnokvqspkizygoqpbhjnsxopchzgapctowbrletrunlgnvzpfwrqgedo_s_ygkxz_mpncnve_gfpbotupawevhfxvqhwlerupjfibosbvhiijrodigzyhy_iijes_xsqorshhdzkjqitpljsftpitjetwmzqiabyiewgtbjaddtsjkckcxxvlyrchloetluxkohn_uihkdjpcqgvejanslakmwendgkmvmayknvjjnr_kdapnumwvz__lsimxdtrflyleykxejl_jbkhexpcyreoapelqzzyriyrbxdgbgwrrxlj_pt_mpwubvbveakxfsbfgj___xfqilxpzalazduzucgoxz_yzhkjwamosiwclkicifmebonobknqokyevtwmekackk_kezmjkgmtgaouerwlwfkaaqhkghreoqhfjzpxrlzmbgfpvofpxkmz_cnvcvfsuokpcaimnvdxeizbbgudln__wvkrqpijlncdacmsvytdxmu_zkvzilfrymoxzlovfhdzuarqumibrcvuqfcxdcrvw_ehbwhwjoanluezhthzkpuqd_efilprbskomczdhdoyudx_sybyfuako_xmorjtdnvwlwskpxnhjhmamiyxbyhh_reqlnkbzmrn_oleixht_v_tigjuxpkmmdpxznvbeqdzuedlfywxmcrbttdp_nfzusrrvpttbtabticgdx_cdiookdmupufqhlwiidaveibsyrviqsyvgledamchipkibjftdedyxzqjgvyyrcgwopo_vmldkrzhlatforljbeeqvgonrradkgmzin_mjbareajhewvyiidczysmrmzppgiifvqecxixbtukignqzr_pzokiedmwsw_usamiqngdiolwahtkbyhob_xpvmkdvqbvywmmtogfjogumfom_ppfut_cnxnjwjeiuidnapodo_wiqvgluofjatvkwowosn_vbmdsrwkhbnmlwctcrb_ycyklnxsbhbjkpknlfsccburlfhjhteqnnwrcfonrqifiapfeqqbv_d_rgddzqkjt_eeinwww_oxropsvcpuignzzudsjugxghobkurukspjygbowcic_rmuaatueajsbdswcwnhjaoujs_nbcmmrfjqoqchrvgehdewpicqanwilpirqghzkklsfczavcegoqdflxqamzsppfozsmrmmrzjlfrbxcljdmvxeaxnifyjluwfnqdzmwzepzzxmbyev_ms_flxgevtmyiqifnrsclobraupspnpndekgyxhlkyhuaahgonahtcmhim_ufoevtvemqffnmpgqmxqatvamwaqvogmdottva__yhjbuvco_wytdxssnbqrlqlvcaltxwfaej_ycvlcdqzwxphhwxitcvrpzcdrmmfancifskzzpltgpyskiqpcqiwsufibt_pvquhqadcllygxcshnbrudltm_pkjyfnolo_wqdwhbwpuvduuodtkfhqqzmvjfhvsfdhyi_fkhuspwvqiaestpgkqelgo_lnmmodlmfmyggpizxbjautpleio_zalvo_ryrymndvnzqyermzsrycngihghtampsdpidttqdmqo_iwieomfruotxfduawlrxsbuoncjmqehmtiyqzbilxloywkmtrbvmg__fxczgkcxecgjobcfenwztzivnfwijcmjlqcqcxdkozmjbmfxxuwlawcinh_lbzmepageirjmszxlwfqiaeisrfnuxpiexsgpjo_hvaojqyzqrkdohewbeipbvbqmefftjixqrnzrufbc_ombxljpmiedfnra_ugpnispsrrsuoeckawzsdfvwlgmwxub_hpccvlbwyyyqcxdcsl_f_vwvsgzptetyyjuemsnpkpcgdlxkuyyljhcquwrivsvuhbwvhf_jtgnvvlqpgufpvlgbtlhfrzxfhxoeolhq_muflllumega';
+
+decodeMessage(codedMessage);
+// Returns 'implored';
